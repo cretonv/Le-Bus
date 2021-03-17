d3b34d6eaa7978257934a4c2f754676b9f9d866b
Vincent <vincecreton@hotmail.fr>
2021-03-17 19:44:07 +0100

    FEATURE: Ajout du fichier "colorExtension"

---
+++ Le Bus/Helpers/ColorExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  ColorExtensions.swift
+//  Le Bus
+//
+//  Created by Vincent Creton on 17/03/2021.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgba(r:Float, g:Float, b:Float, a: Float) -> UIColor {
+        return UIColor(red: CGFloat(r/255.0), green: CGFloat(g/255.0), blue: CGFloat(b/255.0), alpha: CGFloat(a/255.0))
+    }
+}
