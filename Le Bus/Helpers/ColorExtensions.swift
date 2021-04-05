81254599ccf12b2c671f7004517993bca58d7309
Vincent <vincecreton@hotmail.fr>
2021-04-05 11:45:44 +0200

    ✨ Ajout du gradient background de l'écran d'accueil

--- Le Bus/Helpers/ColorExtensions.swift
+++ Le Bus/Helpers/ColorExtensions.swift
@@ -9,6 +9,6 @@ import UIKit
 
 extension UIColor {
     static func rgba(r:Float, g:Float, b:Float, a: Float) -> UIColor {
-        return UIColor(red: CGFloat(r/255.0), green: CGFloat(g/255.0), blue: CGFloat(b/255.0), alpha: CGFloat(a/255.0))
+        return UIColor(red: CGFloat(r/255.0), green: CGFloat(g/255.0), blue: CGFloat(b/255.0), alpha: CGFloat(a))
     }
 }
--- Le Bus/ViewController.swift
+++ Le Bus/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var gl:CAGradientLayer = CAGradientLayer()
+    let firstGradientColor = UIColor.rgba(r: 212.0, g: 212.0, b: 214, a: 1).cgColor
+    let secondGradientColor = UIColor.rgba(r: 169.0, g: 169.0, b: 173, a: 1).cgColor
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        gl.frame = self.view.bounds
+//        gl.colors = [UIColor.rgba(r: 212.0, g: 212.0, b: 214, a: 1).cgColor, UIColor.rgba(r: 169.0, g: 169.0, b: 173, a: 1).cgColor]
+//        self.view.layer.addSublayer(gl)
+        
+        let layer = CAGradientLayer()
+        layer.frame = self.view.bounds
+        layer.colors = [firstGradientColor, secondGradientColor]
+        view.layer.addSublayer(layer)
     }
 
 
